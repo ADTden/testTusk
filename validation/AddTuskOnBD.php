60a3cb5851a798ad7635993d1a1ccf6da15df17a
SkFrozen <frozentw91@gmail.com>
2021-10-08 13:12:58 +0300

    modified with session

--- validation/AddTuskOnBD.php
+++ validation/AddTuskOnBD.php
@@ -1,27 +1,26 @@
 <?php
-include($_SERVER['DOCUMENT_ROOT']."/header.php");
-$userName=htmlspecialchars(trim($_POST[userName]));
-$userEmail=trim($_POST[userEmail]);
-$tuskText=htmlspecialchars(trim($_POST[tuskText]));
+include($_SERVER['DOCUMENT_ROOT'] . "/header.php");
+$userName = htmlspecialchars(trim($_POST['userName']));
+$userEmail = trim($_POST['userEmail']);
+$tuskText = htmlspecialchars(trim($_POST['tuskText']));
 
-if (strlen($userName) ==0 || strlen($userEmail)==0|| strlen($tuskText)==0) {
-      header('Location: /forms/CreateTusk.php/?tusk=errorInput');
-    exit();
+if (strlen($userName) == 0 || strlen($userEmail) == 0 || strlen($tuskText) == 0) {
+	header('Location: /forms/CreateTusk.php/?tusk=errorInput');
+	exit();
 }
-if (!filter_var($_POST[userEmail], FILTER_VALIDATE_EMAIL)) {
-      header('Location: /forms/CreateTusk.php/?tusk=errorEmail');
-    exit();
+if (!filter_var($_POST['userEmail'], FILTER_VALIDATE_EMAIL)) {
+	header('Location: /forms/CreateTusk.php/?tusk=errorEmail');
+	exit();
 }
-if (strlen($_POST[tuskText])>500) {
-    header('Location: /forms/CreateTusk.php/?tusk=errorText');
-    exit();
+if (strlen($_POST['tuskText']) > 500) {
+	header('Location: /forms/CreateTusk.php/?tusk=errorText');
+	exit();
 }
-if (strlen($_POST[userName])>30) {
-    header('Location: /forms/CreateTusk.php/?tusk=errorUser');
-    exit();
+if (strlen($_POST['userName']) > 30) {
+	header('Location: /forms/CreateTusk.php/?tusk=errorUser');
+	exit();
 }
-$query = "INSERT INTO `tusks` (`Name`, `Email`, `Text`, `Status`,`ID`,`redact`) VALUES ('$userName', '$userEmail', '$tuskText', 'В работе',NULL,NULL)";
+$query = ("INSERT INTO `todos_bd` (`Name`, `Email`, `Text`, `Status`,`redact`, `login`, `password`) VALUES ('$userName', '$userEmail', '$tuskText', 'В работе',NULL, NULL, NULL)");
 if ($result = mysqli_query($link, $query)) {
-    header('Location: /?tusk=add');
+	header('Location: /?tusk=add');
 }
-?>
